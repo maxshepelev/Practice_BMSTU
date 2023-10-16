7fefa7a1d6ffaf52d4c35e507e46dc9bb8f17b66
maxshepelev <144708781+maxshepelev@users.noreply.github.com>
2023-10-16 23:26:45 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -8,6 +8,11 @@ class Point_on_the_coordinate_plane:
         self.x_coordinate = x
         self.y_coordinate = y
         
+    #метод str возвращает координаты заданной точки в виде строки
+    
+    def __str__(self):
+        return '(' + str(self.x_coordinate) + ',' + ' ' + str(self.y_coordinate) + ')'
+        
     #данный метод высчитывает расстояния от данной точки до цетра координат
     
     def distance_to_coordinate_center(self):
@@ -48,7 +53,33 @@ class Point_on_the_coordinate_plane:
         else:
             return 'lines coincide'
         
+class Point_in_the_space(Point_on_the_coordinate_plane):
+    
+    #единственные атрибуты точки: координатЫ по оси Ox, по оси Oy и по оси Oz
+    
+    def __init__(self, x, y, z):
+        super().__init__(x, y)
+        self.z_coordinate = z
+        
+    #метод str возвращает координаты заданной точки в виде строки
+    
+    def __str__(self):
+        return '(' + str(self.x_coordinate) + ',' + ' ' + str(self.y_coordinate) + ',' + ' ' + str(self.z_coordinate) + ')'
+        
+    #данный метод высчитывает расстояния от данной точки до цетра координат в пространстве
+    
+    def threeD_distance_to_coordinate_center(self):
+        return (self.x_coordinate ** 2 + self.y_coordinate ** 2 + self.z_coordinate ** 2) ** 0.5
+    
+    #данный метод высчитывает расстояние между двумя выбранными точками в пространстве
+    
+    @staticmethod
+    def threeD_distance_between_two_points(a, b):
+        return ((a.x_coordinate - b.x_coordinate) ** 2 + (a.y_coordinate - b.y_coordinate) ** 2 + (a.z_coordinate - b.z_coordinate) ** 2) ** 0.5        
+        
 a = Point_on_the_coordinate_plane(0, 0)
 b = Point_on_the_coordinate_plane(1, 1)
 c = Point_on_the_coordinate_plane(0, -1)
 d = Point_on_the_coordinate_plane(1, 0)
+e = Point_in_the_space(3, 4, 5)
+f = Point_in_the_space(1, 0, 2)
